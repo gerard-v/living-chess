b2caa6a05c2b6b28d8fbb3808e3a087531e7a931
Timo de Vries <vriestimo@gmail.com>
2018-04-26 19:46:10 +0200

    Turn board into an object

--- chess.py
+++ chess.py
@@ -1,72 +1,47 @@
-# Build the board
-board = [['. ']*8 for i in range(8)]
+class chessboard:
+  # Squares
+  b = [['. ']*8 for i in range(8)]
 
-# Add the pieces
-lineUp = "RNBQKBNR"
-board[0] = [x+'b' for x in lineUp]
-board[1] = ['Pb']*8
-board[6] = ['Pw']*8
-board[7] = [x+'w' for x in lineUp]
+  def __init__(self):
+    # Add the pieces
+    lineUp = "RNBQKBNR"
+    self.b[0] = [x+'b' for x in lineUp]
+    self.b[1] = ['Pb']*8
+    self.b[6] = ['Pw']*8
+    self.b[7] = [x+'w' for x in lineUp]
 
-def printboard():
-  print()
-  for i in range(8):
-    for j in range(8):
-      print(board[i][j], end=' ')
+  def print(self):
     print()
-  print()
+    for i in range(8):
+      for j in range(8):
+        print(self.b[i][j], end=' ')
+      print()
+    print()
 
-def move(origin, destination):
-  board[destination[0]][destination[1]] = board[origin[0]][origin[1]]
-  board[origin[0]][origin[1]] = '. '
+  def move(self, origin, destination):
+    self.b[destination[0]][destination[1]] = self.b[origin[0]][origin[1]]
+    self.b[origin[0]][origin[1]] = '. '
 
-def isvalidpawnmove(origin, destination, color):
-  #TODO: attacking moves, move limit 1 for moves from the 3rd rank and up (or 6th and down), taking "en passant"
-  if color == 'w' and board[origin[0]][origin[1]][1] == color and origin[1]==destination[1] and origin[0]>destination[0] and origin[0]<=destination[0]+2:
-    return True
-  else:
-    if color == 'b' and board[origin[0]][origin[1]][1] == color and origin[1]==destination[1] and origin[0]<destination[0] and origin[0]>=destination[0]-2:
-      return True
-    else:
-      return False
-
-def isvalidmove(origin, destination, color):
-  # origin empty ('. ')
-  if board[origin[0]][origin[1]] == '. ':
-    return False
-  if board[origin[0]][origin[1]][0] == 'P':
-    return isvalidpawnmove(origin,destination,color)
-  
-  return True
-  
-printboard()
+board = chessboard()
+board.print()
 color = 'w'
 print("White to move")
 
 while True :
   origin = input("Move from: ")
   origin = ('87654321'.index(origin[1]),'abcdefgh'.index(origin[0]))
-  #print(origin[0])
-  #print(origin[1])
-
   destination = input("Move to: ")
   destination = ('87654321'.index(destination[1]),'abcdefgh'.index(destination[0]))
-  #print(destination[0])
-  #print(destination[1])
 
-  if isvalidmove(origin,destination,color):
-    move(origin,destination)
-    printboard()
-	  # Switch between black and white
-    if color == 'b':
-      color = 'w'
-      print("White to move")
-    else:
-      color = 'b'
-      print("Black to move")
+  board.move(origin,destination)
+  board.print()
+  # Switch between black and white
+  if color == 'b':
+    color = 'w'
+    print("White to move")
   else:
-    print("Invalid move")
-
+    color = 'b'
+    print("Black to move")
 
 
 
