f59de291d3e5dc6e2fece8443864a647b64caa7c
Timo de Vries <vriestimo@gmail.com>
2019-01-30 18:42:52 +0100

    WIP Add the white king as an object

--- chess.py
+++ chess.py
@@ -1,11 +1,36 @@
 import sys
 from string import digits
 
+class king:
+  def __init__(self, color):
+    assert color in ['black', 'white']
+    self.color = color
+
+  def __str__(self):
+    if (self.color=='white'):
+      return 'K'
+    else:
+      return 'k'
+
+  def wakeUp(self):
+    print("The %s king wakes up" %self.color);
+    fields = board.getAccessibleFields(self)
+    print(fields)
+  # def move()
+    # look around you, which directions have squares?
+    
+    # feel/listen: sense what fields are a no-go
+    
+    # make a move
+    
+
 class chessboard:
   # Squares
   board = [['.']*8 for i in range(8)]
-
+  
+  # only the white king: '8/8/8/8/8/8/8/4K3 w - - 0 1' (with quotes)
   def __init__(self, fen='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'):
+    print(fen);
     # Split FEN record into fields
     fields = fen.split(' ')
     # Set color of player to move
@@ -18,8 +43,24 @@ class chessboard:
         if j in digits:
           x += int(j)
         else:
-          self.board[i][x] = j
+          #self.board[i][x] = j
+          if j=='K':
+            self.board[i][x] = king('white')
           x += 1
+
+    self.whitePieces = []
+    for line in self.board:
+      for i in line:
+        if str(i) in ['RNBQKBNR']:
+          self.whitePieces.append(i)
+
+
+  def dayBreak(self):
+    for p in self.whitePieces:
+      p.wakeUp()
+
+  def nightFall(self):
+    pass
 
   def print(self):
     print()
@@ -28,6 +69,9 @@ class chessboard:
         print(square, end=' ')
       print()
     print()
+
+  def getAccessibleFields(self, piece):
+    pass
 
   def move(self, origin, destination):
     self.board[destination[0]][destination[1]] = self.board[origin[0]][origin[1]]
@@ -40,17 +84,21 @@ else:
 board.print()
 
 while True :
+  input("Press enter to continue day/night cycle.");
+
   if board.color == 'w':
     print("White to move")
+    board.dayBreak()
   else:
     print("Black to move")
+    board.nightFall()
 
-  origin = input("Move from: ")
-  origin = ('87654321'.index(origin[1]),'abcdefgh'.index(origin[0]))
-  destination = input("Move to: ")
-  destination = ('87654321'.index(destination[1]),'abcdefgh'.index(destination[0]))
+  #origin = input("Move from: ")
+  #origin = ('87654321'.index(origin[1]),'abcdefgh'.index(origin[0]))
+  #destination = input("Move to: ")
+  #destination = ('87654321'.index(destination[1]),'abcdefgh'.index(destination[0]))
 
-  board.move(origin,destination)
+  #board.move(origin,destination)
   board.print()
   # Switch between black and white
   if board.color == 'b':
